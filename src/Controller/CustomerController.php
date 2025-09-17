c9b159362bc230bf51dd39db961c801405a990c3
Kalenga243 <kevinkalenga10@gmail.com>
2025-09-17 09:28:56 +0200

    Understanding symfony controllers, routes and layouts

---
+++ src/Controller/CustomerController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CustomerController extends AbstractController
+{
+    #[Route('/customer', name: 'app_customer')]
+    public function index(): Response
+    {
+        return $this->render('customer/index.html.twig');
+    }
+}
