2766008188462ca018b4266655afe0ab1f35c3de
Kalenga243 <kevinkalenga10@gmail.com>
2025-09-18 09:23:43 +0200

    Symfony form - part2

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,8 +4,12 @@ namespace App\Controller;
 
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Product;
+use App\Form\ProductType;
 
 final class ProductController extends AbstractController
 {
@@ -18,4 +22,24 @@ final class ProductController extends AbstractController
             'products' => $products
         ]);
     }
+    #[Route('/admin/add/product', name: 'app_product_add')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $product = new Product();
+        
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $product->setCreatedAt(new \DateTimeImmutable());
+        }
+
+        return $this->render('admin/product/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    
 }
--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -10,6 +10,8 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+
 
 class ProductType extends AbstractType
 {
